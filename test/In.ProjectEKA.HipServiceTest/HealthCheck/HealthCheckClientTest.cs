504d696d463227336edad75253e320a0275acf92
charles161 <fortloop.charles@gmail.com>
2020-08-17 17:57:42 +0530

    HIP45 | Bhavadharani, Charles | Add OpemMRS Health Check Client  - To check OpenMRS Endpoints

--- test/In.ProjectEKA.HipServiceTest/HealthCheck/HealthCheckClientTest.cs
+++ test/In.ProjectEKA.HipServiceTest/HealthCheck/HealthCheckClientTest.cs
@@ -7,11 +7,12 @@ namespace In.ProjectEKA.HipServiceTest.OpenMrs {
     using System;
     using FluentAssertions;
     using In.ProjectEKA.HipService.OpenMrs;
+    using Moq.Protected;
     using Moq;
     using Xunit;
 
     [Collection ("Health Check Client Tests")]
-    public class HealthCareClientTest {
+    public class HealthCheckClientTest {
         private Mock<IHealthCheckClient> healthCheckClient;
 
         [Fact]
@@ -25,5 +26,89 @@ namespace In.ProjectEKA.HipServiceTest.OpenMrs {
             Assert.True (result.GetType ().Equals (new Dictionary<string, string> ().GetType ()));
         }
 
+        [Fact]
+        private async void ShouldReturnServiceAsHealthyWhenServiceHasStatus200 () {
+            var handlerMock = new Mock<HttpMessageHandler> (MockBehavior.Strict);
+            var httpClient = new HttpClient (handlerMock.Object);
+            var openmrsConfiguration = new OpenMrsConfiguration {
+                Url = "https://someurl/openmrs/",
+                Username = "someusername",
+                Password = "somepassword"
+            };
+            var openmrsClient = new OpenMrsClient (httpClient, openmrsConfiguration);
+            handlerMock
+                .Protected ()
+                .Setup<Task<HttpResponseMessage>> (
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage> (),
+                    ItExpr.IsAny<CancellationToken> ()
+                )
+                .ReturnsAsync (new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.OK
+                })
+                .Verifiable ();
+            OpenMrsHealthCheckClient openMrsHealthCheckClient = new OpenMrsHealthCheckClient (new Dictionary<string, string> { { "Service", "path/to/resource" }
+            }, openmrsClient);
+
+            var result = await openMrsHealthCheckClient.CheckHealth ();
+
+            Assert.True (result["Service"].Equals ("Healthy"));
+        }
+
+        [Fact]
+        private async void ShouldReturnServiceAsUnhealthyWhenServiceHasStatus400 () {
+            var handlerMock = new Mock<HttpMessageHandler> (MockBehavior.Strict);
+            var httpClient = new HttpClient (handlerMock.Object);
+            var openmrsConfiguration = new OpenMrsConfiguration {
+                Url = "https://someurl/openmrs/",
+                Username = "someusername",
+                Password = "somepassword"
+            };
+            var openmrsClient = new OpenMrsClient (httpClient, openmrsConfiguration);
+            handlerMock
+                .Protected ()
+                .Setup<Task<HttpResponseMessage>> (
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage> (),
+                    ItExpr.IsAny<CancellationToken> ()
+                )
+                .ReturnsAsync (new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.BadRequest
+                })
+                .Verifiable ();
+            OpenMrsHealthCheckClient openMrsHealthCheckClient = new OpenMrsHealthCheckClient (new Dictionary<string, string> { { "Service", "path/to/resource" }
+            }, openmrsClient);
+
+            var result = await openMrsHealthCheckClient.CheckHealth ();
+
+            Assert.True (result["Service"].Equals ("Unhealthy"));
+        }
+
+        [Fact]
+        private async void ShouldReturnServiceAsUnhealthyWhenRequestThrowsException () {
+            var handlerMock = new Mock<HttpMessageHandler> (MockBehavior.Strict);
+            var httpClient = new HttpClient (handlerMock.Object);
+            var openmrsConfiguration = new OpenMrsConfiguration {
+                Url = "https://someurl/openmrs/",
+                Username = "someusername",
+                Password = "somepassword"
+            };
+            var openmrsClient = new OpenMrsClient (httpClient, openmrsConfiguration);
+            handlerMock
+                .Protected ()
+                .Setup<Task<HttpResponseMessage>> (
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage> (),
+                    ItExpr.IsAny<CancellationToken> ()
+                )
+                .ThrowsAsync (new Exception ("Throw some exception"))
+                .Verifiable ();
+            OpenMrsHealthCheckClient openMrsHealthCheckClient = new OpenMrsHealthCheckClient (new Dictionary<string, string> { { "Service", "path/to/resource" }
+            }, openmrsClient);
+
+            var result = await openMrsHealthCheckClient.CheckHealth ();
+
+            Assert.True (result["Service"].Equals ("Unhealthy"));
+        }
     }
 }
